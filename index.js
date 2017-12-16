d57f5631724a5dbead69947d163641976f64159b
bridgs <bridgs.dev@gmail.com>
2017-12-16 03:27:28 -0500

    Just doing the basic babel shenanigans

---
+++ index.js
@@ -0,0 +1,40 @@
+const cards = [
+	'green bean casserole',
+	'turkey',
+	'ham',
+	'salt & pepper',
+	'buns',
+	'butter',
+	'cranberry sauce',
+	'carrots',
+	'brussels sprouts',
+	'salad'
+];
+
+let gameState = {
+	config: {},
+	deck: [ 'b', 'a', 'b' ],
+	discard: [ 'c' ],
+	players: [
+		{
+			hand: [ 'a', 'b' ],
+			table: [ 'c' ],
+			plate: [ 'b' ],
+			tummy: [ [ 'a', 'b' ] ]
+		},
+		{
+			hand: [],
+			table: [],
+			plate: [],
+			tummy: []
+		},
+		{
+			hand: [],
+			table: [],
+			plate: [],
+			tummy: []
+		}
+	]
+};
+
+console.log('hi');
